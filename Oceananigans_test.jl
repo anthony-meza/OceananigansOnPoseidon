acc8e21fb0d4ca61445402b69b696817401a5ae2
anthony-meza <ameza98@outlook.com>
2024-07-12 10:46:45 -0400

    small fix

--- Oceananigans_test.jl
+++ Oceananigans_test.jl
@@ -7,7 +7,7 @@ Pkg.instantiate()
 using Oceananigans, Test
 grid = RectilinearGrid(GPU(), size=(128, 128), x=(0, 2π), y=(0, 2π), topology=(Periodic, Periodic, Flat))
 model = NonhydrostaticModel(; grid, advection=WENO())
-ϵ(x, y) = 2rand() - 1
+ϵ(x, y, z) = 2rand() - 1
 set!(model, u=ϵ, v=ϵ)
 simulation = Simulation(model; Δt=0.01, stop_time=4)
 run!(simulation)
